97f1cc01d0953eeec425f41b763adc273e8f555a
dustySingh <dusty_encoder1993@outlook.com>
2016-10-22 23:48:03 +0530

    mazor changes done...!!!

--- TrainingSetGeneration.m
+++ TrainingSetGeneration.m
@@ -1,39 +1,38 @@
 function Ts = TrainingSetGeneration()
 
-close all;
-Ts = zeros(1000,3);
+Ts = zeros(10000,3);
 
 for i=1:1000
     Ts(i,1) = rand;
     Ts(i,2) = rand;
 end
 
-for i=1:1000
+for i=1:10000
    if(Ts(i,1)<=0.25)
        if(Ts(i,2)<=0.25)
-           Ts(i,3) = 0.15;
+           Ts(i,3) = 0.05;
        else
-           Ts(i,3)=0.85;
+           Ts(i,3)=0.95;
        end
    else
        if(Ts(i,2)<=0.25)
-           Ts(i,3) = 0.85;
+           Ts(i,3) = 0.95;
        else
-           Ts(i,3)=0.15;clc
+           Ts(i,3)=0.05;
        end
    end
 end
 
-plot(0,0)
-hold
-for i=1:1000
-    if(Ts(i,3)>0.5)
-        plot(Ts(i,1), Ts(i,2), 'r*');
-    else
-        plot(Ts(i,1), Ts(i,2), 'bo');
-    end
-end
+% plot(0,0)
+% hold
+% for i=1:10000
+%     if(Ts(i,3)>0.5)
+%         plot(Ts(i,1), Ts(i,2), 'r*');
+%     else
+%         plot(Ts(i,1), Ts(i,2), 'bo');
+%     end
+% end
 
 %lgd = legend('LOW', 'HIGH');
-
+%hold off;
 end
---
+++ plotTrainingSet.m
@@ -0,0 +1,15 @@
+function plotTrainingSet(Ts);
+    close all;
+    N = length(Ts);
+    
+    plot(0,0)
+    hold
+    for i=1:N
+        if(Ts(i,3)>0.25)
+            plot(Ts(i,1), Ts(i,2), 'r*');
+        else
+            plot(Ts(i,1), Ts(i,2), 'bo');
+        end
+    end
+    hold off;
+end
